960307eeaf9ae2a545b37fdb1d0b0e1d2f7c544a
KONEYard <KONEYard@users.noreply.github.com>
2022-06-25 21:39:11 +0000

    section1.php

---
+++ src/portion/section1.php
@@ -0,0 +1,46 @@
+<section class="section-1">
+
+                <div class="head">
+                    <h3 class="heading text-uppercase">Acheter avec <span class="orange text_bold ">nous</span></h3>
+                    <h5 class="text_muted text-center">Le top des trucs du shopping</h5>
+                </div>
+                <div class="wrapper">
+                    <a href="#">
+                        <div class="box">
+                            <img src="assets/image/clothe/complet-f-gris.png" alt="shopper | ">
+                            <h4 class="title">Complet femme</h4>
+                        </div>
+                    </a>
+                    <a href="#">
+                        <div class="box">
+                            <img src="assets/image/electronic/clay_banks_tqytwfn1b_kS7Cm.jpg" alt="shopper |">
+                            <h4 class="title">Ordinateur Apple Bureau</h4>
+                        </div>
+                    </a>
+                    <a href="#">
+                        <div class="box">
+                            <img src="assets/image/plant/cactus.jpg" alt="shopper |">
+                            <h4 class="title"> Plante Cactus</h4>
+                        </div>
+                    </a>
+                    <a href="#">
+                        <div class="box">
+                            <img src="assets/image/clothe/complet-homme.png" alt="shopper |">
+                            <h4 class="title">Complet Homme</h4>
+                        </div>
+                    </a>
+                    <a href="#">
+                        <div class="box">
+                            <img src="assets/image/electronic/julian_o_hayon_hy3l4_2yfhX.jpg" alt="shopper |">
+                            <h4 class="title">Pc Apple + mobile + montre</h4>
+                        </div>
+                    </a>
+                    <a href="#">
+                        <div class="box">
+                            <img src="assets/image/clothe/pull-adidas.jpg" alt="shopper |">
+                            <h4 class="title">Pull addidas</h4>
+                        </div>
+                    </a>
+
+                </div>
+            </section>
